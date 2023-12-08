f2d2ae0441555e088f155c4517ec64e71dff297e
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-08 20:31:40 +0200

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import List, Sequence, Tuple, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return types of typing module classes"""
     return [(i, len(i)) for i in lst]
