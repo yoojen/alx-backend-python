3d32390db01309ebcebee285dff5339c7799e2bb
yoojen <eugeneemma7@gmail.com>
2024-01-08 21:33:27 +0200

    checked if ther is KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
         """check if all output are right"""
         res = access_nested_map(nested_map, sequence)
         self.assertEqual(res, expected_output)
+
+    @parameterized.expand([
+            ({}, ['a'], KeyError),
+            ({"a": 1}, ['a', 'b'], KeyError)
+            ])
+    def test_access_nested_map_exception(self, nested_map, sequence, exc):
+        """check if the function raises an exception"""
+        with self.assertRaises(exc, msg="KeyError Exception occured"):
+            access_nested_map(nested_map, sequence)
